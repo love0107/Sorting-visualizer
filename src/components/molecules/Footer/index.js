26d13166953486431532a8070f1da5f4ed58d236
love0107 <lovlesh0107@gmail.com>
2022-04-25 19:46:05 +0530

    project completed

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+    return (
+        <footer className="Footer">
+            <section>
+                {/* future code */}
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
